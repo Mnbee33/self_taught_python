2359bc92f26efa5670681966b275143dd11100a8
Manabe Misato <mnbee33@gmail.com>
2019-05-25 17:09:19 +0900

    commit chapter21

---
+++ chapter21/stack.py
@@ -0,0 +1,44 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return not self.items
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[-1]
+
+    def size(self):
+        return len(self.items)
+
+stack = Stack()
+print(stack.is_empty())
+
+stack.push(1)
+print(stack.is_empty())
+
+item = stack.pop()
+print(item)
+print(stack.is_empty())
+
+for i in range(0, 6):
+    stack.push(i)
+
+print(stack.peek())
+print(stack.size())
+
+word_stack = Stack()
+for c in "Hello":
+    word_stack.push(c)
+
+reverse = ""
+while word_stack.size():
+    reverse += word_stack.pop()
+
+print(reverse)
